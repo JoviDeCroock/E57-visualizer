ec01c7a416678aae12fad05f0bd3d8dbacb61f92
Jovi De Croock <decroockjovi@gmail.com>
2020-07-29 17:27:20 +0200

    convert to three.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,54 +1,14 @@
-const canvas = document.getElementById('my-canvas');
-const gl = canvas.getContext("webgl");
+import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
 
-if (gl === null) {
-  throw new Error('WebGL not available');
-}
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-function createShader (gl, sourceCode, type) {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, sourceCode);
-  gl.compileShader(shader);
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw new Error('Could not compile WebGL program. \n\n' + gl.getShaderInfoLog(shader));
-  }
-
-  return shader;
-}
-
-gl.clearColor(0.0, 0.0, 0.0, 1.0);gl.clear(gl.COLOR_BUFFER_BIT);
-
-var vertCode = 'attribute vec3 coordinates;'+
-'attribute vec3 color;'+
-'varying vec3 vColor;'+
-'void main(void) {' +
-   ' gl_Position = vec4(coordinates, 1.0);' +
-   'vColor = color;'+
-'}';
-
-
-var fragCode = 'precision mediump float;'+
-'varying vec3 vColor;'+
-'void main(void) {'+
-    'gl_FragColor = vec4(vColor, 1.);'+
-'}';
-
-const fragmentShader = createShader(gl, fragCode, gl.FRAGMENT_SHADER)
-const vertexShader = createShader(gl, vertCode, gl.VERTEX_SHADER)
-
-const program = gl.createProgram(vertexShader, fragmentShader);
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
-gl.useProgram(program);
-
-
-if (!gl.getProgramParameter( program, gl.LINK_STATUS)) {
-  var info = gl.getProgramInfoLog(program);
-  throw new Error('Could not compile WebGL program. \n\n' + info);
-}
+var scene = new THREE.Scene();
 
 fetch('http://localhost:3000/parse', { credentials: 'omit' })
   .then(res => {
@@ -62,8 +22,8 @@ fetch('http://localhost:3000/parse', { credentials: 'omit' })
     data.forEach((scan, i) => {
       const amountOfPoints = scan.x.length;
 
-      const vertices = [];
-      const colors = [];
+      const geometry = new THREE.Geometry();
+      console.log(geometry);
       for (let i = 0;i < amountOfPoints; i++) {
         const x = scan.x[i];
         const y = scan.y[i];
@@ -72,24 +32,12 @@ fetch('http://localhost:3000/parse', { credentials: 'omit' })
         const green = scan.green[i];
         const blue = scan.blue[i];
 
-        vertices.push(x, y, z);
-        colors.push(red, green, blue)
+        geometry.vertices.push(new THREE.Vector3(x, y, z))
+        geometry.colors.push(new THREE.Color(red, green, blue));
       }
-
-      const verticesIndecesBuffer = gl.createBuffer();
-      const points = new Float32Array(vertices);
-
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, verticesIndecesBuffer);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, points, gl.STATIC_DRAW);
-
-      const colorBuffer = gl.createBuffer();
-      const colorsRay = new Float32Array(vertices);
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, colorsRay, gl.STATIC_DRAW);
-
-      console.log('drawing ', vertices.length, ' points');
-      console.log('drawing ', colors.length, ' colors');
-      gl.drawElements(gl.POINTS, vertices.length, gl.UNSIGNED_SHORT, 0);
+      const material = new THREE.PointsMaterial( { size: 1, vertexColors: THREE.VertexColors } );
+      const cloud = new THREE.Line(geometry, material);
+      scene.add(cloud);
     });
+    renderer.render( scene, camera );
   })
