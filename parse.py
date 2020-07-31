8b867a90348fd5fbe5df1aead99af383189305c8
Jovi De Croock <decroockjovi@gmail.com>
2020-07-31 11:06:46 +0200

    qol

--- parse.py
+++ parse.py
@@ -1,23 +1,18 @@
 import pye57, json, numpy
 
 data = pye57.E57("./pump.e57")
-count = data.scan_count;
+count = data.scan_count
 
 for i in range(0, count - 1):
   header = data.get_header(i);
 
   scan = data.read_scan_raw(i);
-  scan["cartesianX"] = scan["cartesianX"].tolist()
-  scan["cartesianY"] = scan["cartesianY"].tolist()
-  scan["cartesianZ"] = scan["cartesianZ"].tolist()
-  scan["intensity"] = scan["intensity"].tolist()
-  scan["colorRed"] = scan["colorRed"].tolist()
-  scan["colorGreen"] = scan["colorGreen"].tolist()
-  scan["colorBlue"] = scan["colorBlue"].tolist()
-  scan["rowIndex"] = scan["rowIndex"].tolist()
-  scan["columnIndex"] = scan["columnIndex"].tolist()
-  scan["cartesianInvalidState"] = scan["cartesianInvalidState"].tolist()
 
-  print(json.dumps(scan));
-
-data3d = data.data3d;
+  print(json.dumps({
+    'cartesianX': scan["cartesianX"].tolist(),
+    'cartesianY': scan["cartesianY"].tolist(),
+    'cartesianZ': scan["cartesianZ"].tolist(),
+    'colorRed': scan["colorRed"].tolist(),
+    'colorGreen': scan["colorGreen"].tolist(),
+    'colorBlue': scan["colorBlue"].tolist(),
+  }));
--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
